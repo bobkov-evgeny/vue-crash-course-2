69c3d6d42e5de8c3e2c6aacc46028885014a4931
Evgeny_Bobkov <freeedayy@gmail.com>
2021-07-03 16:46:53 +1000

    ...

--- app.js
+++ app.js
@@ -26,3 +26,65 @@ const app = Vue.createApp({
 });
 
 app.mount("#app");
+
+const inputRub = document.querySelector('#rub');
+const inputUsd = document.querySelector('#usd');
+
+inputRub.addEventListener('input', () => {
+	const request = new XMLHttpRequest();
+
+	request.open(`GET`, 'current.json');
+	request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+	request.send();
+
+	request.addEventListener('readystatechange', () => {
+		if(request.readyState === 4 && request.status === 200) {
+			const data = JSON.parse(request.response).current.usd;
+			inputUsd.value = (+inputRub.value / data).toFixed(2);
+		} else {
+			inputUsd.value = 'Что-то пошло не так';
+		}
+	});
+});
+
+const form = document.querySelector('form');
+const message = {
+	loading: 'Загрузка',
+	success: 'Спасибо! Скоро мы с Вами свяжемся',
+	failure: 'Что-то пошло не так...',
+};
+
+function postData(form) {
+	form.addEventListener('submit', function(e) {
+		e.preventDefault();
+
+		const statusMessage = document.createElement('div');
+		statusMessage.classList.add('status');
+		statusMessage.textContent = message.loading;
+		form.append(statusMessage);
+
+		const request = new XMLHttpRequest();
+		request.open('POST', 'server.php');
+		
+		request.setRequestHeader('Content-type', 'application/json');
+		const formData = new FormData(form);
+		const object = {};
+		formData.forEach((value,key) => object[key] = value);
+		const json = JSON.stringify(object);
+		
+
+		request.send(json);
+		request.addEventListener('load', () => {
+			if(request.status === 200) {
+				console.log(request.response);
+				statusMessage.textContent = message.success;
+				form.reset();
+				setTimeout(() => {statusMessage.remove();}, 2000);
+			} else {
+				statusMessage.textContent = message.failure;
+			}
+		});
+		
+	});
+}
+postData(form);
